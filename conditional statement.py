787ef68c87ab2f38acfb2550667f57de378b51ca
InnoEmma <thatis771.com>
2022-08-04 16:54:57 +0100

    my first commit

---
+++ conditional statement.py
@@ -0,0 +1,31 @@
+#print ( 5==5)
+#a=6 == 5
+#print(a)
+
+
+##print('pssword123' == 'password')
+
+#print (5 == int ('5'))
+
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a = [1,3,2] == [1,2,3]
+print (a)
+
+b= 2 in [1,3,2]
+print (b)
+
